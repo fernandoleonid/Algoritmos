dfbf877299fb792a5746287cd02f8a0aef2ab07b
unknown <fernandoleonid@gmail.com>
2019-02-22 09:30:08 -0300

    Cadastro Turma A

--- 2019 - A/03 - vetores/cadastro.js
+++ 2019 - A/03 - vetores/cadastro.js
@@ -6,21 +6,60 @@ const limparFormulario = () =>{
 }
 
 const exibirCadastro = () => {
-   alert (bd);
+   const $tabelaCadastro = $("#tabelaCadastro");
+   const ultimoIndice = bd.length - 1
+   $tabelaCadastro.innerHTML = "";
+   for (let i=0; i<=ultimoIndice; i++){
+      $tabelaCadastro.innerHTML += `${i} - ${bd[i]} <br>`;
+   }
 }
 
 const adicionarAluno = ()  => {
 
    const nome = $("#nome").value;
-   bd.push(nome);
-   limparFormulario();
+   if (nome == "" ){
+      alert("Digite um nome para cadastrar!")
+   }else{
+      bd.push(nome);
+      limparFormulario();
+      exibirCadastro();
+   }
+}
+
+const excluirAluno = () => {
+   const nome = prompt("Digite um nome para exluir!");
+   const indice = bd.indexOf(nome);
+   const inexistente = -1;
+
+   if ( indice == inexistente ) {
+      alert ("Aluno não encontrado")
+   }else{
+      bd.splice(indice,1);
+   }
+
    exibirCadastro();
 }
 
+const atualizarAluno = () => {
+   const nome = prompt("Digite um nome para atualizar!");
+   if (nome){
+      const nomeAtualizado = prompt ("Digite o novo nome!");
+      if (nomeAtualizado){
+         const indice = bd.indexOf(nome);
+         const inexistente = -1;
+
+         if ( indice == inexistente ) {
+            alert ("Aluno não encontrado")
+         }else{
+            bd.splice(indice,1,nomeAtualizado);
+         }
+         exibirCadastro();
+      }
+   }
+}
+
 $("#adicionar").addEventListener("click",adicionarAluno);
-
-
-
-( ) => { }
+$("#excluir").addEventListener("click",excluirAluno);
+$("#atualizar").addEventListener("click",atualizarAluno)
 
 
